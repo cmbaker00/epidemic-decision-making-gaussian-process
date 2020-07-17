f4f9a1486246be8f9d357aeb5586a1aeb70ece3b
cbaker1 <cbaker1@unimelb.edu.au>
2020-07-17 16:05:50 +1000

    Added functionality to pull out key statistics from SIR model

--- models/epi_models_basic.py
+++ models/epi_models_basic.py
@@ -16,7 +16,7 @@ class BasicSIR:
                 self.gamma*infected]
         return dydt
 
-    def solve_ode(self, t_end, t_steps=10, return_full_timeseries=False):
+    def solve_system(self, t_end, t_steps=10, return_full_timeseries=False):
         t = np.linspace(0, t_end, t_steps)
         sol = odeint(self.ode_system, self.initial_condition, t)
         if return_full_timeseries:
@@ -24,10 +24,18 @@ class BasicSIR:
         else:
             return sol[-1]
 
+def generate_epidemic_stats(model, stat='AR10'):
+    if stat == 'AR10':
+        s, i, r = model.solve_system(10)
+        return r/(s + i + r)
+
+
 
 if __name__ == "__main__":
     ode = BasicSIR(beta=.005, gamma=.2, initial_condition=[1000, 1, 0])
-    print(ode.solve_ode(10))
-    tvals, solution = ode.solve_ode(t_end=10, t_steps=1000, return_full_timeseries=True)
+    print(ode.solve_system(10))
+    tvals, solution = ode.solve_system(t_end=10, t_steps=1000, return_full_timeseries=True)
     plt.plot(tvals, solution)
     plt.show()
+
+    print(generate_epidemic_stats(ode, stat='AR10'))
