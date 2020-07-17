337b0276579796cab73d5209624ae6c2423cf583
cbaker1 <cbaker1@unimelb.edu.au>
2020-07-17 18:28:46 +1000

    Started integrating model into the emulator

--- emulator.py
+++ emulator.py
@@ -1,5 +1,15 @@
-
+import models.epi_models_basic as epi
 
 class Emulator:
     def __init__(self, model, parameters, name):
         pass
+
+
+if __name__ == "__main__":
+    emulator_test = Emulator(epi.BasicSIR,
+                             {'beta':[0, 1],
+                              'gamma':[0, 1],
+                              'initial_condition': [999, 1, 0]},
+                             'epi_SIR_test'
+    )
+
