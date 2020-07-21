2672d2cb7b0878ec05e766c0a98db0981e73ecbd
cbaker1 <cbaker1@unimelb.edu.au>
2020-07-21 12:48:52 +1000

    added gamma distribution input for parameter values

--- emulator.py
+++ emulator.py
@@ -1,6 +1,7 @@
 import models.epi_models_basic as epi
 import random
 import scipy.stats as stats
+import numpy.random as np_rand
 
 
 class Emulator:
@@ -18,6 +19,16 @@ class Emulator:
             elif type == 'uniform':
                 value_min, value_max = attribute['value']
                 value = random.uniform(value_min, value_max)
+            elif type == 'gamma':
+                # k shape, theta scale
+                # mean = k*theta
+                # var = k theta**2
+                # theta = var/mean
+                # k = mean/theta
+                value_mean, value_var = attribute['value']
+                theta = value_var/value_mean
+                k = value_mean/theta
+                value = np_rand.gamma(shape=k, scale=theta)
             else:
                 raise ValueError("Parameter input type {} is not valid"
                                  .format(type))
@@ -32,13 +43,14 @@ if __name__ == "__main__":
     emulator_test = Emulator(
         model=epi.run_sir_model,
         parameters_range={
-            'beta': {'value': [0, 1], 'type': 'uniform'},
+            'beta': {'value': [0.005, 0.00001], 'type': 'gamma'},
             'gamma': {'value': [0, 1], 'type': 'uniform'},
             'initial_condition': {'value': [999, 1, 0], 'type': 'point'}
                           },
         name='epi_SIR_test'
     )
-    params = emulator_test.gen_parameters()
-    print(params)
-    emulator_test.run_model(params)
+    for i in range(3):
+        params = emulator_test.gen_parameters()
+        print(params)
+        emulator_test.run_model(params)
 
