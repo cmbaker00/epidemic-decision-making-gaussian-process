59f3a05beafc8db05140b8c27251878e811c70e7
cbaker1 <cbaker1@unimelb.edu.au>
2020-10-19 13:12:11 +1100

    Added the stochastic SEIR model with quarantine and hospital

---
+++ models/epi_model_example.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import count
+from numpy.random import random
+from functools import lru_cache
+
+class DiseaseDynamics:
+    def __init__(self, pop_size, init_infected,
+                 beta, gamma, incubation_pr, hosp_rate,
+                 test_percentage):
+
+        self.total_pop = pop_size
+        self.initial_infected = init_infected
+        self.beta = beta
+        self.gamma = gamma
+        self.incubation_pr = incubation_pr
+        self.hosp_rate = hosp_rate
+        self.test_percentage = test_percentage
+
+        self.current_time = 0
+        self.time_list = [self.current_time]
+
+        states = ('susceptible', 'exposed',
+                  'exposed_q', 'infected',
+                  'infected_q', 'infected_h',
+                  'recovered')
+
+        self.state_names = states
+
+        self.states_index_dict = {
+            state: i for state, i in zip(states, count())
+        }
+
+        self.state_short_list = ('s', 'e',
+                            'eq', 'i',
+                            'iq', 'ih',
+                            'r')
+
+        population_state = [0]*self.total_pop
+        for i in range(self.initial_infected):
+            population_state[i] = self.states_index_dict['infected']
+        self.population_state = np.array(population_state)
+
+        self.state_totals = self.calculate_state_totals(as_list = True)
+
+    def run_one_time_step(self):
+        self.current_time += 1
+
+        pop = self.population_state
+        state_updates = np.zeros(pop.shape)
+
+        s_index = self.states_index_dict['susceptible']
+
+        perc_s = sum(pop == self.states_index_dict['susceptible'])/self.total_pop
+        num_i = sum(pop == self.states_index_dict['infected'])
+
+        # new exposures
+        pr_infected = self.beta*num_i/self.total_pop
+        new_exposure = np.array(random(pop.shape) < pr_infected) & np.array(pop == self.state_index('susceptible'))
+        state_updates[new_exposure] = self.state_increment('susceptible', 'exposed')
+
+        # exposed to infected
+        new_infected = np.array(random(pop.shape) < self.incubation_pr) & np.array(pop == self.state_index('e'))
+        state_updates[new_infected] = self.state_increment('e', 'i')
+
+        # exposed quarantine to infected quarantine
+        new_infected = np.array(random(pop.shape) < self.incubation_pr) & np.array(pop == self.state_index('eq'))
+        state_updates[new_infected] = self.state_increment('eq', 'iq')
+
+        # infected to hospital
+        new_hospital = np.array(random(pop.shape) < self.hosp_rate) & np.array(pop == self.state_index('i'))
+        state_updates[new_hospital] = self.state_increment('i', 'ih')
+
+        # infected quarantine to hospital
+        new_hospital = np.array(random(pop.shape) < self.hosp_rate) & np.array(pop == self.state_index('iq'))
+        state_updates[new_hospital] = self.state_increment('iq', 'ih')
+
+        # infected to recovered
+        new_recovered = np.array(random(pop.shape) < self.gamma) & np.array(pop == self.state_index('i'))
+        state_updates[new_recovered] = self.state_increment('i', 'r')
+
+        # infected quarantine to recovered
+        new_recovered = np.array(random(pop.shape) < self.gamma) & np.array(pop == self.state_index('iq'))
+        state_updates[new_recovered] = self.state_increment('iq', 'r')
+
+        # infected hospital to recovered
+        new_recovered = np.array(random(pop.shape) < self.gamma/2) & np.array(pop == self.state_index('ih'))
+        state_updates[new_recovered] = self.state_increment('ih', 'r')
+
+        # tested
+        people_tested = np.array(random(pop.shape) < self.test_percentage)
+        exposed_tested = people_tested & np.array(pop == self.state_index('e'))
+        infected_tested = people_tested & np.array(pop == self.state_index('i'))
+        state_updates[exposed_tested] = self.state_increment('e', 'eq')
+        state_updates[infected_tested] = self.state_increment('i', 'iq')
+
+        pop += state_updates.astype(int)
+        self.population_state = pop
+
+        self.append_population_state()
+
+    def calculate_state_totals(self, as_list=False):
+        state_totals = [sum(self.population_state == self.state_index(state_name)) for state_name in self.state_names]
+        if as_list:
+            state_totals = [[total] for total in state_totals]
+        total_dict = {state_name: total for state_name, total in zip(self.state_names, state_totals)}
+        total_dict['time'] = [self.current_time] if as_list else self.current_time
+        return total_dict
+
+    def append_population_state(self):
+        old_totals = self.state_totals
+        new_totals = self.calculate_state_totals()
+        for state_name in new_totals.keys():
+            old_totals[state_name].append(new_totals[state_name])
+
+    def state_increment(self, from_state, to_state):
+        init_index = self.state_index(from_state)
+        final_index = self.state_index(to_state)
+        return final_index - init_index
+
+    def state_index(self, current_state):
+        if len(current_state) < 4:
+            current_state = self.short_to_long_state(current_state)
+        return self.states_index_dict[current_state]
+
+    @lru_cache()
+    def short_to_long_state(self, state_name):
+        short_to_long_dict = {short_name: long_name for short_name, long_name in
+                              zip(self.state_short_list, self.states_index_dict.keys())}
+        return short_to_long_dict[state_name]
+
+
+if __name__ == "__main__":
+    epi = DiseaseDynamics(pop_size=5000,
+                          init_infected=5,
+                          beta=.1,
+                          gamma=.05,
+                          incubation_pr=.2,
+                          hosp_rate=.01,
+                          test_percentage=.008)
+
+    for i in range(400):
+        epi.run_one_time_step()
+
+    print(epi.state_totals)
+    print(epi.calculate_state_totals())
+    plt.plot(epi.state_totals['infected_h'])
+    plt.plot(epi.state_totals['infected'])
+    plt.plot(epi.state_totals['infected_q'])
+    plt.legend(['Hospital active cases',
+                'Mild active cases',
+                'Quarantine active cases'])
+    plt.show()
